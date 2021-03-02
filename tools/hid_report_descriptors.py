e49aac44ab0c3b9b15ee7953666354a7b41711be
Thomas Cousin <thomas.cousin@gmail.com>
2021-03-02 11:43:35 +0100

    Add usb hid descriptor for spacemouse

--- tools/hid_report_descriptors.py
+++ tools/hid_report_descriptors.py
@@ -21,6 +21,7 @@ from adafruit_usb_descriptor import hid
 DeviceData = namedtuple('DeviceData', ('report_length', 'out_report_length', 'usage_page', 'usage'))
 HID_DEVICE_DATA = {
     "KEYBOARD" : DeviceData(report_length=8, out_report_length=1, usage_page=0x01, usage=0x06),    # Generic Desktop, Keyboard
+    "SPACEMOUSE" : DeviceData(report_length=8, out_report_length=1, usage_page=0x01, usage=0x08),
     "MOUSE" : DeviceData(report_length=4, out_report_length=0, usage_page=0x01, usage=0x02),       # Generic Desktop, Mouse
     "CONSUMER" : DeviceData(report_length=2, out_report_length=0, usage_page=0x0C, usage=0x01),    # Consumer, Consumer Control
     "SYS_CONTROL" : DeviceData(report_length=1, out_report_length=0, usage_page=0x01, usage=0x80), # Generic Desktop, Sys Control
@@ -69,6 +70,64 @@ def keyboard_hid_descriptor(report_id):
              0x91, 0x01,                 #   Output (Constant)
              0xC0,                       # End Collection
             )))
+
+def spacemouse_hid_descriptor(report_id):
+    data = HID_DEVICE_DATA["SPACEMOUSE"]
+    return hid.ReportDescriptor(
+        description="SPACEMOUSE",
+        report_descriptor=bytes(
+            0x05, 0x01,           #  Usage Page (Generic Desktop)  
+            0x09, 0x08,           #  0x08: Usage (Multi-Axis Controller)  
+            0xA1, 0x01,           #  Collection (Application)  
+            0xa1, 0x00,            # Collection (Physical)
+            0x85, 0x01,         #  Report ID 
+            0x16, 0x0c, 0xfe,              #     LOGICAL_MINIMUM (-500) 
+            0x26, 0xf4, 0x01,              #     LOGICAL_MAXIMUM (500) 
+            0x36, 0x00, 0x80,              #     PHYSICAL_MINIMUM (-32768) 
+            0x46, 0xff, 0x7f,              #     PHYSICAL_MAXIMUM (32767)
+            0x09, 0x30,           #    Usage (X)  
+            0x09, 0x31,           #    Usage (Y)  
+            0x09, 0x32,           #    Usage (Z)  
+            0x09, 0x33,           #    Usage (RX)  
+            0x09, 0x34,           #    Usage (RY)  
+            0x09, 0x35,           #    Usage (RZ)  
+            0x75, 0x10,           #    Report Size (16)  
+            0x95, 0x06,           #    Report Count (6)  
+            0x81, 0x02,           #    Input (variable,absolute)  
+            0xa1, 0x02,                         #   COLLECTION (logical)     
+            0x09, 0x52,                         #    USAGE (Input Mode)         
+            0x15, 0x00,                         #    LOGICAL_MINIMUM (0)      
+            0x25, 0x0a,                         #    LOGICAL_MAXIMUM (255)
+            0x75, 0x08,                         #    REPORT_SIZE (8)         
+            0x95, 0x01,                         #    REPORT_COUNT (8)         
+            0xb1, 0x02,                         #    FEATURE (Data,Var,Abs    
+            0xc0,                               #   END_COLLECTION
+            0xC0,                           #  End Collection  
+            0xa1, 0x00,            # Collection (Physical)
+            0x85, 0x03,         #  Report ID 
+            0x15, 0x00,           #   Logical Minimum (0)  
+            0x25, 0x01,           #    Logical Maximum (1) 
+            0x75, 0x01,           #    Report Size (1)  
+            0x95, 15,           #    Report Count (24) 
+            0x05, 0x09,           #    Usage Page (Button)  
+            0x19, 1,           #    Usage Minimum (Button #1)  
+            0x29, 15,           #    Usage Maximum (Button #24)  
+            0x81, 0x02,           #    Input (variable,absolute)  
+            0x95, 0x01,          #     REPORT_COUNT = 1
+            0x75, 32-15,          #     REPORT_SIZE = 5
+            0x81, 0x03,          #     INPUT = Cnst,Var,Abs
+            0xC0,
+            0xa1, 0x00,           # Collection (Physical)
+            0x85, 0x04,           #  Report ID 
+            0x05, 0x08,       #   USAGE_PAGE (LEDs)  
+            0x19, 0x01,       #   USAGE_MINIMUM (1)  
+            0x29, 0x08,       #   USAGE_MAXIMUM (8) 
+            0x95, 0x08,       #   REPORT_COUNT (8)  
+            0x75, 0x01,       #   REPORT_SIZE (1)  
+            0x91, 0x02,         #   OUTPUT (Data,Var,Abs)     
+            0xC0,
+            0xC0                       # End Collection
+            )))            
 
 def mouse_hid_descriptor(report_id):
     data = HID_DEVICE_DATA["MOUSE"]
